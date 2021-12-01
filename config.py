34a3de09015821d34c2c3bb3ba53d1bb281235a2
Niko Känsäkoski <niko.kansakoski@student.oulu.fi>
2021-12-01 09:35:50 +0200

    updated config

--- config.py
+++ config.py
@@ -8,10 +8,14 @@ check_urls = True
 check_sender = True
 check_body = True
 
-keyword_count = 5
+keyword_count = 3
 
 domain_staff = "oulu.fi"
 domain_student = "student.oulu.fi"
 
 special_characters = [",", ".", "/", "\"", ";", "-", "_", "!", "?", "(", ")", "--", ".\"", "!--", ",\"", ".--", "'", ":", "*", '""', '"', "``", "''", '’', '—', '‘']
 
+search_beginning = "https://www.oulu.fi/fi/search?search_api_fulltext="
+search_end = "&field_targeting=All"
+
+crawl_base = "https://www.oulu.fi"
