798afb0486edadcd21b010e0752a3cfc3499d4c8
Niko Känsäkoski <niko.kansakoski@student.oulu.fi>
2021-12-02 12:03:38 +0200

    another option for webcrawling

---
+++ webcrawler.py
@@ -0,0 +1,29 @@
+import config
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import requests
+
+
+def crawl(searchwords: list):
+    if len(searchwords) != 0:
+        for searchword in searchwords:
+            search = config.search_beginning + searchword + config.search_end
+            page = requests.get(search)
+
+            soup = BeautifulSoup(page.content, "html.parser")
+            newUrls = []
+            for link in soup.find_all("a", "teaser"):
+                print(link.get("href"))
+
+                newUrls.append(link.get("href"))
+
+            for url in newUrls:
+                page = requests.get(url)
+
+                soup = BeautifulSoup(page.content, "html.parser")
+
+                for hit in soup.find_all(attrs={'class': 'body'}):
+                    text = hit.text.strip()
+                    print(text)
+
+
