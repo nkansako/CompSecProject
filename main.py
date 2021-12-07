3609996f557e01c408ec76534de92d701dba3b51
Niko Känsäkoski <niko.kansakoski@student.oulu.fi>
2021-12-07 12:28:06 +0200

    almost functional main loop

--- main.py
+++ main.py
@@ -19,73 +19,69 @@ def main():
     #init_logger()
     messages = mail.getMail(account)
     print("Got ",len(messages)," messages.")
-    #for i in range(len(messages) or 20):
-    #    body = messages[i].body
-    #    msgid = messages[i].message_id
-    #    database.db_insert(conn,cur,body,msgid)
-    #get = database.db_get(conn,cur)
-    #print("DB GET succesful!")
-
 
     logging.info("Parsing emails")
+
+    add_messages_to_database(messages)
+
+    check_emails()
+
+
+def add_messages_to_database(messages):
     parsed = mailparser.parse_all_emails(messages)
     logging.info("Emails parsed")
     for email in parsed:
         # email["text"] = nlp.remove_stop_words(email["text"]) can use to remove extra stuff if needed
         text = email["text"]
         ex_score = nlp.score_exclamation_marks(text)
-        textwosw = nlp.remove_stop_words(text,3)
+        textwosw = nlp.remove_stop_words(text, 3)
         keywords = nlp.find_keywords(textwosw)
-        print("Text without editing:",text)
-        print("Text with stopwords removed:",textwosw)
-        print("keywords from nlp:",keywords)
-        #web_crawler(keywords)
+        print("Text without editing:", text)
+        print("Text with stopwords removed:", textwosw)
+        print("keywords from nlp:", keywords)
         body = text
+        body = body.replace(u'\xa0', u' ')
         msgid = email["m_id"]
         sender = email["email"]
         links = email["links"]
         attachments = email["attachment_names"]
-        database.db_insert(conn,cur,body,msgid,sender,links,ex_score,attachments,keywords)
-
-    get = database.db_get(conn,cur)
-    for i in range(1): #change this to len(get) to iterate all. also remember add bool for already crawled msgs
-        parsedGet = database.parseGet(get[i])
-        #for kw in parsedGet["keywords"]:
-        print("Crawling for keywords:",parsedGet["keywords"],"...")
-        tmp = webcrawler.crawl(parsedGet["keywords"])
-        print("Crawled and got return:",tmp)
-        #print(parsedGet["links"])
-        tmp2 = 0
-        matchj = 0
-        for j in range(len(tmp)):
-            print("Crossreferencing crawl with email body...")
-            #print("keyword:",tmp[j][1])
-            crefscore = nlp.cross_reference(tmp[j][1],parsedGet["body"])
-            if crefscore > tmp2:
-                crefscore = tmp2
-                matchj = j
-        #print("\n\n\n\n\nBest match with crawling: Keyword ",tmp[matchj][1],"\n\nRelates to:",parsedGet["body"])
+        database.db_insert(conn, cur, body, msgid, sender, links, ex_score, attachments, keywords)
 
 
+def check_emails():
+    get = database.db_get(conn, cur)
+    for i in range(len(get)):
+        parsedGet = database.parseGet(get)
+        if parsedGet["checked"] == 0:
+            sender_val = nlp.check_domain(parsedGet["sender"])
+            if sender_val:
+                print("This email came from a university staff member or another student\nWhile this makes the email more likely to not be malicious, do not rely on only this information, the email could also be stolen!")
+            else:
+                print("This email came from outside of the university!\nWhile this does not make the sender malicious, remain cautious!")
+            for link in parsedGet["links"]:
+                url_value = phishcheck.check_url(link)
+                if url_value > 0.8:
+                    print("Link: ", link, " is fishy ")
+                url_value2 = nlp.check_link(link)
+                if url_value2:
+                    print("This link: ", link, " should be from university website")
+                else:
+                    print("This link: ", link, " should be from outside of the university website")
+            for attachment in parsedGet["attachments"]:
+                attachment_val = nlp.dummy_check_attachments(attachment)
+                if attachment_val == 1.0:
+                    print("Nothing fishy here in attachment: ", attachment, " file type should be safe")
+                elif attachment_val == 0.5:
+                    print("Something may be fishy about attachment: ", attachment,
+                          " file type is often used maliciously")
+                else:
+                    print("This file is very suspicious, do not open the file! Attachment: ", attachment)
+            #if nlp.check_sender_name(parsedGet["sender"]):
 
-def check_file():
-    # TODO write something that checks a file if malicious
-    pass
+              #  print("Crawling for keywords:", parsedGet["keywords"], "...")
+              #  tmp = webcrawler.crawl(parsedGet["keywords"])
+              #  print("Crawled and got return:", tmp)
 
-
-def check_url():
-    # TODO write something that checks url if phishing site etc
-    pass
-
-
-def find_keywords():
-    # TODO write something that finds most common words in the body of the email
-    pass
-
-
-def web_crawler(keywords: list) -> list:
-    # TODO write something that crawls university website for possible matches in email
-    return []
 
 
 def init_logger():
@@ -140,8 +136,9 @@ def run_tests():
 
     database.db_close(conn1)
 
+
 if __name__ == "__main__":
-    run_tests()
+    #run_tests()
     startTime = time.perf_counter()
     #authenticate, get account token
     account = mail.authenticate()
@@ -151,6 +148,6 @@ if __name__ == "__main__":
     cur = database.create_cursor(conn)
     database.format_table(conn,cur)
     #call main()
-    #main()
+    main()
     #close DB
     database.db_close(conn)
